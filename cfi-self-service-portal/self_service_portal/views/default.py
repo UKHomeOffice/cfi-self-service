caf6043057ee1ef1ab50e4f0ab518fbc202794cc
ryanjackson1HO <ryan.jackson@digital.homeoffice.gov.uk>
2023-12-15 09:53:13 +0000

    This commit features an updated app template and routes and tests for the Request Environment Access feature.

--- cfi-self-service-portal/self_service_portal/views/default.py
+++ cfi-self-service-portal/self_service_portal/views/default.py
@@ -2,4 +2,8 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='self_service_portal:templates/home.jinja2')
 def home_view(request):
-    return {'project': 'Self Service Portal'}
+    return {'project': 'Self Service Portal', 'title': 'Welcome', 'description': 'Welcome to the self service user portal!'}
+
+@view_config(route_name='env-request', renderer='self_service_portal:templates/env-request.jinja2')
+def env_request_view(request):
+    return {'project': 'Self Service Portal', 'title': 'Request Environment Access', 'description': 'Here you can make a request for access to an environment.'}
--- cfi-self-service-portal/tests/test_functional.py
+++ cfi-self-service-portal/tests/test_functional.py
@@ -2,6 +2,10 @@ def test_root(testapp):
     res = testapp.get('/', status=200)
     assert res.status_code == 200
 
+def test_env_request(testapp):
+    res = testapp.get('/env/request', status=200)
+    assert res.status_code == 200
+
 def test_notfound(testapp):
     res = testapp.get('/badurl', status=404)
     assert res.status_code == 404
