62d37d17f0965710739d28fa7fc31d34acdcb264
ryanjackson1HO <ryan.jackson@digital.homeoffice.gov.uk>
2024-01-10 09:14:43 +0000

    Tweaked to generate a random secret key - more work will be done on this as part of a future ticket.

--- cfi-self-service-portal/self_service_portal/__init__.py
+++ cfi-self-service-portal/self_service_portal/__init__.py
@@ -1,10 +1,11 @@
-
+import secrets
 from pyramid.session import SignedCookieSessionFactory
 from pyramid.config import Configurator
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application. """
-    session_factory = SignedCookieSessionFactory('your_secret_key')
+    secret_key = secrets.token_hex(32)
+    session_factory = SignedCookieSessionFactory(secret_key)
     with Configurator(settings=settings) as config:
         config.set_session_factory(session_factory)
         config.include('pyramid_jinja2')
