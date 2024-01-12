94f9f7487a159fe3270e8f661ffd47a7a6d65407
ryanjackson1HO <ryan.jackson@digital.homeoffice.gov.uk>
2024-01-12 09:38:37 +0000

    Added request filters card and functionality into the request dashboard.

--- cfi-self-service-portal/self_service_portal/views/default.py
+++ cfi-self-service-portal/self_service_portal/views/default.py
@@ -20,22 +20,17 @@ def home_view(request):
 
 @view_config(route_name='env-dashboard', renderer='self_service_portal:templates/env_access/env_dashboard.jinja2')
 def env_dashboard_view(request):
-
     # Pop the flash message up on return on HTTP POST:
     messages = request.session.pop_flash()
-
     # Create the connection to the DynamoDB table:
     dynamodb_table = Models.get_dynamodb_connection("")
-
     form_data = request.params
     selected_status = form_data.get('status')
     selected_environment = form_data.get('environment')
-
     # Define condition expressions and attribute values based on the selected filters:
     condition_expression = None
     expression_attribute_names = {}
     expression_attribute_values = {}
-
     if selected_status:
         condition_expression = '#access_status = :status'
         expression_attribute_names['#access_status'] = 'access-status'
@@ -48,7 +43,6 @@ def env_dashboard_view(request):
             condition_expression = environment_condition
         expression_attribute_names['#access_environment'] = 'access-environment'
         expression_attribute_values[':environment'] = selected_environment
-
     # Perform a query to fetch records from DynamoDB with the conditions:
     if selected_status or selected_environment:
         response = dynamodb_table.scan(
@@ -58,18 +52,14 @@ def env_dashboard_view(request):
         )
     else:
         response = dynamodb_table.scan()
-
-    ############################################################################
-
+    # Arrange the records by status and request date:
     items = response.get('Items', [])
     sorted_items = sorted(items, key=lambda x: ( x.get('access-status', ''), datetime.strptime(x.get('access-request-date'), '%d/%m/%Y %H:%M') ), reverse=True)
-
     # Setup integers to hold status counts:
     all_requests_count = 0
     pending_requests = 0
     approved_requests = 0
     denied_requests = 0
-
     # Get the counts for each request status:
     for item in items:
         all_requests_count += 1
@@ -99,9 +89,7 @@ def env_dashboard_view(request):
 
 @view_config(route_name='env-new-request', renderer='self_service_portal:templates/env_access/env_new_request.jinja2')
 def env_new_request_view(request):
-
     if request.method == "POST":
-
         # Output the form data to an JSON object ready for adding to the DynamoDB table:
         form_data = request.params
         item = {
@@ -115,17 +103,14 @@ def env_new_request_view(request):
             'access-request-date': datetime.now().strftime("%d/%m/%Y %H:%M"),
             'access-comments': form_data.get('requestComments'),
         }
-
         # Create the connection to the DynamoDB table and add the new request record:
         dynamodb_table = Models.get_dynamodb_connection("")
         dynamodb_table.put_item(
             Item=item
         )
-
         # Use the HTTPFound exception to perform a redirection back to the dashboard:
         request.session.flash('Record Submitted')
         redirect_url = request.route_url('env-dashboard')
-
         raise HTTPFound(redirect_url)
 
     return {
@@ -139,16 +124,12 @@ def env_new_request_view(request):
 
 @view_config(route_name='env-request', renderer='self_service_portal:templates/env_access/env_existing_request.jinja2')
 def env_request_view(request):
-
     # Find the ID required for the DynamoDB query:
     request_id = request.matchdict['id']
-
     # Connect to the DynamoDB table:
     dynamodb_table = Models.get_dynamodb_connection("")
-
     # Update the DynamoDB record when admin responds to a request:
     if request.method == "POST":
-
         # Specify the update expression and attribute values:
         form_data = request.params
         update_expression = "SET #access_status = :access_form_status, #admin_comments = :admin_form_comments, #admin_full_name = :admin_form_full_name, #admin_response_date = :admin_form_response_date, #notification_alert = :notification_alert"
@@ -166,7 +147,6 @@ def env_request_view(request):
             ':admin_form_response_date': datetime.now().strftime("%d/%m/%Y %H:%M"),
             ':notification_alert': 'false',
         }
-
         # Update the item in the table:
         dynamodb_table.update_item(
             Key={
@@ -176,15 +156,12 @@ def env_request_view(request):
             ExpressionAttributeNames=expression_attribute_names,
             ExpressionAttributeValues=expression_attribute_values
         )
-
         # Create flash message to post back to page:
         request.session.flash('Record Updated')
-
     # Obtain the DynamoDB record required:
     response = dynamodb_table.query(
         KeyConditionExpression=Key('Request-ID').eq(request_id)
     )
-
     # Assign matched record data to a list:
     for item in response['Items']:
         first_name            = item.get('access-first-name')
@@ -198,7 +175,6 @@ def env_request_view(request):
         admin_name            = item.get('admin-full-name')
         admin_response_date   = item.get('admin-response-date')
         admin_comments        = item.get('admin-comments')
-
     # Pop the flash message up on return on HTTP POST:
     messages = request.session.pop_flash()
 
@@ -228,20 +204,14 @@ def env_admin_control_panel_view(request):
 
     # Find the ID required for the DynamoDB query:
     request_id = request.matchdict['id']
-
     # Connect to the DynamoDB table:
     dynamodb_table = Models.get_dynamodb_connection("")
 
     if request.method == "POST":
-
         # Specify the update expression and attribute values:
         form_data = request.params
-
-        ###########################################################################
-
         # Update Record:
         if form_data.get('AdminControlPanel') == "Update":
-
             # Specify the update expression and attribute values:
             form_data = request.params
             update_expression = "SET #access_email_address = :access_email_address, #access_environment = :access_environment, #access_first_name = :access_first_name, #access_last_name = :access_last_name, #access_request_date = :access_request_date, #access_status = :access_status, #access_team = :access_team, #admin_full_name = :admin_full_name, #admin_comments = :admin_comments, #admin_response_date = :admin_response_date, #notification_alert = :notification_alert"
@@ -271,7 +241,6 @@ def env_admin_control_panel_view(request):
                 ':admin_response_date': datetime.now().strftime("%d/%m/%Y %H:%M"),
                 ':notification_alert': 'false',
             }
-
             # Update the item in the table:
             dynamodb_table.update_item(
                 Key={
@@ -281,36 +250,25 @@ def env_admin_control_panel_view(request):
                 ExpressionAttributeNames=expression_attribute_names,
                 ExpressionAttributeValues=expression_attribute_values
             )
-
             # Create flash message to post back to page:
             request.session.flash('Record Updated')
-
-        ###########################################################################
-
         # Delete Record:
         if form_data.get('AdminControlPanel') == "Delete":
-
             # Delete the DynamoDB table record:
             response = dynamodb_table.delete_item(
                 Key={
                     'Request-ID': request_id
                 }
             )
-
             # Create flash message to post back to page:
             request.session.flash('Record Deleted')
-
             # Use the HTTPFound exception to perform a redirection back to the dashboard:
             redirect_url = request.route_url('env-dashboard')
             raise HTTPFound(redirect_url)
-
-        ###########################################################################
-
     # Obtain the DynamoDB record required:
     response = dynamodb_table.query(
         KeyConditionExpression=Key('Request-ID').eq(request_id)
     )
-
     # Assign matched record data to a list:
     for item in response['Items']:
         first_name            = item.get('access-first-name')
@@ -324,7 +282,6 @@ def env_admin_control_panel_view(request):
         admin_name            = item.get('admin-full-name')
         admin_response_date   = item.get('admin-response-date')
         admin_comments        = item.get('admin-comments')
-
     # Pop the flash message up on return on HTTP POST:
     messages = request.session.pop_flash()
 
