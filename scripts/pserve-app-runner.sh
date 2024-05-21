e1d305cd1b428d4eab9a1f2ded2332179d73e4f9
ryanjackson1HO <ryan.jackson@digital.homeoffice.gov.uk>
2024-05-21 10:46:36 +0100

    Pipeline and configuration file changes to resolve ongoing issues with start-command failing.

--- scripts/pserve-app-runner.sh
+++ scripts/pserve-app-runner.sh
@@ -2,8 +2,6 @@
 
 # Reset our environment variable for a new virtual environment:
 export VENV=~/env/cfi-self-service/
-# Change directory into your project:
-cd ..
 # Create a new virtual environment:
 python3 -m venv $VENV
 # Wire your shell to use the virtual environment:
